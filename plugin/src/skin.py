e18c04daee24abb0b33790ce10ff93f2c1f1da44
hosein rostami <hosein232r@gmailcom>
2026-07-05 16:20:38 +0330

    Update UI skin and optimize image assets

--- plugin/src/skin.py
+++ plugin/src/skin.py
@@ -1,53 +1,95 @@
 # -*- coding: utf-8 -*-
 
-"""
-OpenTunnel Skin
-"""
-
 MAIN_SKIN = """
 <screen name="OpenTunnelScreen"
     position="center,center"
-    size="620,440"
+    size="760,500"
     title="OpenTunnel">
 
-    <widget name="config"
-        position="10,10"
-        size="600,330"
-        scrollbarMode="showOnDemand"/>
+    <!-- Logo -->
+    <ePixmap
+        pixmap="/usr/lib/enigma2/python/Plugins/Extensions/OpenTunnel/images/logo.png"
+        position="220,10"
+        size="320,120"
+        alphatest="blend" />
 
-    <widget name="status"
-        position="15,350"
-        size="590,25"
-        font="Regular;22"
-        halign="center"/>
+    <!-- Config List -->
+    <widget
+        name="config"
+        position="20,140"
+        size="720,250"
+        scrollbarMode="showOnDemand" />
 
-    <widget source="key_red"
+    <!-- Status Icon -->
+    <ePixmap
+        pixmap="/usr/lib/enigma2/python/Plugins/Extensions/OpenTunnel/images/disconnected.png"
+        position="20,405"
+        size="32,32"
+        alphatest="blend" />
+
+    <!-- Status Text -->
+    <widget
+        source="status"
         render="Label"
-        position="20,390"
-        size="120,30"
-        font="Regular;22"
-        halign="center"/>
+        position="60,405"
+        size="250,32"
+        font="Regular;22" />
 
-    <widget source="key_green"
-        render="Label"
-        position="170,390"
-        size="120,30"
-        font="Regular;22"
-        halign="center"/>
+    <!-- Red -->
+    <ePixmap
+        pixmap="/usr/lib/enigma2/python/Plugins/Extensions/OpenTunnel/images/red.png"
+        position="20,450"
+        size="48,48"
+        alphatest="blend" />
 
-    <widget source="key_yellow"
+    <widget
+        source="key_red"
         render="Label"
-        position="320,390"
-        size="120,30"
-        font="Regular;22"
-        halign="center"/>
+        position="72,458"
+        size="90,24"
+        font="Regular;20" />
 
-    <widget source="key_blue"
+    <!-- Green -->
+    <ePixmap
+        pixmap="/usr/lib/enigma2/python/Plugins/Extensions/OpenTunnel/images/green.png"
+        position="180,450"
+        size="48,48"
+        alphatest="blend" />
+
+    <widget
+        source="key_green"
         render="Label"
-        position="470,390"
-        size="120,30"
-        font="Regular;22"
-        halign="center"/>
+        position="232,458"
+        size="90,24"
+        font="Regular;20" />
+
+    <!-- Yellow -->
+    <ePixmap
+        pixmap="/usr/lib/enigma2/python/Plugins/Extensions/OpenTunnel/images/yellow.png"
+        position="340,450"
+        size="48,48"
+        alphatest="blend" />
+
+    <widget
+        source="key_yellow"
+        render="Label"
+        position="392,458"
+        size="90,24"
+        font="Regular;20" />
+
+    <!-- Blue -->
+    <ePixmap
+        pixmap="/usr/lib/enigma2/python/Plugins/Extensions/OpenTunnel/images/blue.png"
+        position="500,450"
+        size="48,48"
+        alphatest="blend" />
+
+    <widget
+        source="key_blue"
+        render="Label"
+        position="552,458"
+        size="120,24"
+        font="Regular;20" />
 
 </screen>
 """
